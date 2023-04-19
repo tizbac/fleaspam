acd3ea5bc35258936bed71225f5ba057b36bfa22
Tiziano Bacocco <tizbac2@gmail.com>
2023-04-20 01:03:13 +0200

    VERSION{v3.5}

--- modinfo.lua
+++ modinfo.lua
@@ -5,7 +5,7 @@ return
   shortname='TA',
   version = '$VERSION',
   numdependencies=1,
-  depend = { 'Tech Annihilation test-10978-966fcac', },
+  depend = { 'Tech Annihilation test-11004-a47ac3f', },
   mutator='',
   game='',
   shortGame='TA',
